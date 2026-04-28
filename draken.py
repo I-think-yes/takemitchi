70c7de89fc244850c62f2b27e7af384443748df3
buscatojiezon1-eng <buscato.jiezon1@gmail.com>
2026-04-28 04:28:56 +0000

    2nd

--- draken.py
+++ draken.py
@@ -2,5 +2,23 @@ try:
     f = open("mikey.txt" , "x")
     f.write("shopping list manager")
     f.close()
-except FileExitsError:
+except fileExistsError: 
     print("ERROR")
+
+items = []
+
+while True:
+    print("\nMENU")
+    print("1. Add")
+    print("2. Edit")
+    print("3. Delete")
+    print("4. Show")
+    print("5. Exit")
+
+    choice = input("Choose (1-5): ")
+
+    if choice == "1":
+        new_item = input("Enter item to add: ")
+        items.append(new_item)
+        print("Item added!")
+
